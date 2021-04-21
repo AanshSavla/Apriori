06cad462acdda8338e5e1869ee265b3eec115f2b
aanshsavla2453@gmail.com <aanshsavla2453@gmail.com>
2021-04-21 17:58:01 +0530

    Made changes.

--- HITS.py
+++ HITS.py
@@ -7,7 +7,7 @@ Created on Wed Apr 14 07:32:42 2021
 
 import networkx as nx
 import matplotlib.pyplot as plt
- 
+#Creation of graph 
 G = nx.DiGraph()
  
 G.add_edges_from([('A', 'D'), ('B', 'C'), ('B', 'E'), ('C', 'A'),
@@ -17,10 +17,13 @@ G.add_edges_from([('A', 'D'), ('B', 'C'), ('B', 'E'), ('C', 'A'),
  
 plt.figure(figsize =(10, 10))
 nx.draw_networkx(G, with_labels = True)
+#plot graph
  
 hubs, authorities = nx.hits(G, max_iter = 50, normalized = True)
 # The in-built hits function returns two dictionaries keyed by nodes
 # containing hub scores and authority scores respectively.
  
-print("Hub Scores: ", hubs)
+
+#printing hubs
 print("Authority Scores: ", authorities)
+#printing authority scores
